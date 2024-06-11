25c8fbd7a0472fc0a8923e32872e37185776ebbb
sbx_user1051 <sbx_user1051@169.254.35.37>
2024-06-11 22:53:08 +0000

    Tue Jun 11 22:53:08 UTC 2024

--- spec/SUM/sum_spec.rb
+++ spec/SUM/sum_spec.rb
@@ -1,2 +1,9 @@
 require_solution 'SUM'
 
+describe Sum do
+    let(:first_integer) { 2 }
+    let(:second_integer) { 100 }
+    it "Sum two integers" do
+      expect(Sum.new.sum).to eq 102
+    end
+  end
