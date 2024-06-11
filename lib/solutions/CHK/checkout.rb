7afd00b038863e1d5cc1c3d56da0994e5edd9be6
sbx_user1051 <sbx_user1051@169.254.35.37>
2024-06-11 23:32:19 +0000

    Tue Jun 11 23:32:19 UTC 2024

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,6 +1,13 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  PRICE_TABLE = {
+    A: { special_offer: { amount: 3, price: 130 } },
+    B: { special_offer: { amount: 2, price: 45 } },
+    C: {},
+    D: {}
+  }
+
   def checkout(skus)
     # I need to see the input... It's not clear to me the format.
     -1
--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -46,5 +46,21 @@ describe Checkout do
         end
       end
     end
+
+    context 'and it has multple items of different kinds'
+      context 'without special offer'
+        let(:skus) { "ABCACDDCCDD" }
+        it "Return the correct value" do
+          expect(subject).to eq 270
+        end
+      end
+
+      context 'with special offer'
+        let(:skus) { "ABCACDADCACDBBD" }
+        it "Return the correct value" do
+          expect(subject).to eq 395
+        end
+      end
+    end
   end
 end
