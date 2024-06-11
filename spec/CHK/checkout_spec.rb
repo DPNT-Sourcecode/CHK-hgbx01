56c14e950fd194831a9b59b0b6e678c95a2f5eb8
sbx_user1051 <sbx_user1051@169.254.35.37>
2024-06-11 23:23:19 +0000

    Tue Jun 11 23:23:19 UTC 2024

--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -1,10 +1,18 @@
 require_solution 'CHK'
 
 describe Checkout do
+  subject { Checkout.new.checkout(skus) }
   context 'When the SKUs list is empty'
     let(:skus) { "" }
     it "Return the correct value" do
-      expect(Checkout.new.checkout(skus)).to eq 0
+      expect(subject).to eq 0
+    end
+  end
+
+  context 'When the SKUs list has an invalid characters'
+    let(:skus) { "a" }
+    it "Return the error code" do
+      expect(subject).to eq -1
     end
   end
 end
