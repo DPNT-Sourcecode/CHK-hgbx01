9d6a85f8753df3d42ed39dbf1f45427ed5ecc7f5
sbx_user1051 <sbx_user1051@169.254.35.37>
2024-06-12 00:46:10 +0000

    Wed Jun 12 00:46:10 UTC 2024

--- lib/solutions/CHK/check.rb
+++ lib/solutions/CHK/check.rb
@@ -16,3 +16,31 @@ end
 class Item
     attr_accessor %i[amount total_price]
 end
+
+class SpecialOffer
+    def apply(check)
+        raise 'Not implemented'
+    end
+end
+
+class Discount < SpecialOffer
+    attr_accessor %i[amount new_price]
+
+    def apply(check)
+        new_check = check
+        return new_check
+    end
+end
+
+class Gift < SpecialOffer
+    attr_accessor %i[amount item_name]
+
+    def initialize(items)
+        @items = items
+    end
+
+    def apply(check)
+        new_check = check
+        return new_check
+    end
+end
