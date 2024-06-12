f8b0b0674f3d5f481e01bf619f8edb4b33a41f72
sbx_user1051 <sbx_user1051@169.254.35.37>
2024-06-12 01:52:10 +0000

    Wed Jun 12 01:52:10 UTC 2024

--- lib/solutions/CHK/check.rb
+++ lib/solutions/CHK/check.rb
@@ -1,94 +1,102 @@
-class Check
-    attr_accessor :items
+# class Check
+#     attr_accessor :items
 
-    # def add_items(sku, amount)
-    #     items[sku.name] ||= Item.new(0, sku)
-    #     items[sku.name].amount += amount
-    # end
+#     # def add_items(sku, amount)
+#     #     items[sku.name] ||= Item.new(0, sku)
+#     #     items[sku.name].amount += amount
+#     # end
 
-    # def remove_items(sku, amount)
-    #     items[sku.name] ||= Item.new(0, sku)
-    #     current_amount = items[sku.name].amount
-    #     items[sku.name].amount = max(current_amount - amount, 0)
-    # end
+#     # def remove_items(sku, amount)
+#     #     items[sku.name] ||= Item.new(0, sku)
+#     #     current_amount = items[sku.name].amount
+#     #     items[sku.name].amount = max(current_amount - amount, 0)
+#     # end
 
-    def calculate(skus) {
-      items.each do |name, item|
-        item.calculate()
-      end
-      items.sum(&:)
-    }
+#     def calculate(skus) {
+#       items.each do |name, item|
+#         item.calculate()
+#       end
+#       items.sum(&:)
+#     }
 
-    # if sku.key? :special_offers
-    #   calculate_special_offers(check)
-    # else
-    #   check[item][:total_price] += sku[:price] * check[item][:amount]
-    # end
-end
+#     # if sku.key? :special_offers
+#     #   calculate_special_offers(check)
+#     # else
+#     #   check[item][:total_price] += sku[:price] * check[item][:amount]
+#     # end
+# end
 
-class Sku
-    attr_accessor %i[name base_price discounts, gifts]
+# class Sku
+#     attr_accessor %i[name base_price discounts, gifts]
 
-    def initialize(name, base_price, discounts, gifts)
-        @name = name
-        @base_price = base_price
-        @discounts = discounts
-        @gifts = gifts
-    end
+#     def initialize(name, base_price, discounts, gifts)
+#         @name = name
+#         @base_price = base_price
+#         @discounts = discounts
+#         @gifts = gifts
+#     end
 
-    def apply_special_offers(items)
-    end
-end
+#     def apply_special_offers(items)
+#         apply_discount(items)
+#         apply_gifts(items)
+#     end
 
-class Item
-    attr_accessor %i[amount sku total]
+#     def apply_discount(items)
+#         remaining_amount = item
+#         discounts = discounts.sort_by { |discount| discount.amount }
+#         discounts.each
+#     end
 
-    def initialize(amount, sku)
-        @amount = amount
-        @sku = sku
-        @price = 0
-    end
+#     def apply_gifts(items)
+#     end
+# end
 
-    def apply_special_offers(check)
+# class Item
+#     attr_accessor %i[amount sku total]
 
-    end
-end
+#     def initialize(amount, sku)
+#         @amount = amount
+#         @sku = sku
+#         @price = 0
+#     end
+# end
 
-class SpecialOffer
-    def apply_discount(check)
-        raise 'Not implemented'
-    end
+# class SpecialOffer
+#     def apply_discount(check)
+#         raise 'Not implemented'
+#     end
 
-    def apply_gifts
+#     def apply_gifts
 
-    end
-end
+#     end
+# end
 
-class Discount < SpecialOffer
-    attr_accessor %i[amount new_price]
+# class Discount < SpecialOffer
+#     attr_accessor %i[amount new_price]
 
-    def initialize(amount, new_price)
-        @amount = amount
-        @new_price = new_price
-    end
+#     def initialize(amount, new_price)
+#         @amount = amount
+#         @new_price = new_price
+#     end
 
-    def apply(check)
-        new_check = check
-        return new_check
-    end
-end
+#     def apply(check)
+#         new_check = check
+#         return new_check
+#     end
+# end
 
-class Gift < SpecialOffer
-    attr_accessor %i[amount item_name]
+# class Gift < SpecialOffer
+#     attr_accessor %i[amount item_name]
 
-    def initialize(amount, item_name)
-        @amount = amount
-        @item_name = item_name
-    end
+#     def initialize(amount, item_name)
+#         @amount = amount
+#         @item_name = item_name
+#     end
 
-    def apply(check)
-        new_check = check
-        return new_check
-    end
-end
+#     def apply(check)
+#         new_check = check
+#         return new_check
+#     end
+# end
+
 
--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,77 +1,74 @@
-# # noinspection RubyUnusedLocalVariable
+# # # noinspection RubyUnusedLocalVariable
 
-# PRICE_TABLE = {
-#   A: { price: 50, special_offers: { discounts: [ { amount: 5, price: 200 }, { amount: 3, price: 130 } ] } },
-#   B: { price: 30, special_offers: { discounts: [ { amount: 2, price: 45 } ] } },
-#   C: { price: 20 },
-#   D: { price: 15 },
-#   E: { price: 40, special_offers: { gifts: [ { amount: 2, gift: :B } ] } },
-# }
+# # PRICE_TABLE = {
+# #   A: { price: 50, special_offers: { discounts: [ { amount: 5, price: 200 }, { amount: 3, price: 130 } ] } },
+# #   B: { price: 30, special_offers: { discounts: [ { amount: 2, price: 45 } ] } },
+# #   C: { price: 20 },
+# #   D: { price: 15 },
+# #   E: { price: 40, special_offers: { gifts: [ { amount: 2, gift: :B } ] } },
+# # }
 
 
-class Checkout
-  attr_accessor %i[items supported_skus]
+# class Checkout
+#   attr_accessor %i[items supported_skus]
 
-  def initialize
-    initialize_skus
-    @items = {}
-  end
+#   def initialize
+#     initialize_skus
+#     @items = {}
+#   end
 
-  def checkout(input_skus)
-    supported_skus.each do |item,  sku|
-      check.add_items(sku, input_skus.count(item.to_s))
-    end
+#   def checkout(input_skus)
+#     supported_skus.each do |item,  sku|
+#       check.add_items(sku, input_skus.count(item.to_s))
+#     end
 
-    (check.items_amount == input_skus.length) ? check.calculate : -1
-  end
+#     (check.items_amount == input_skus.length) ? check.calculate : -1
+#   end
 
-  private
+#   private
 
-# +------+-------+------------------------+
-# | Item | Price | Special offers         |
-# +------+-------+------------------------+
-# | A    | 50    | 3A for 130, 5A for 200 |
-# | B    | 30    | 2B for 45              |
-# | C    | 20    |                        |
-# | D    | 15    |                        |
-# | E    | 40    | 2E get one B free      |
-# +------+-------+------------------------+
-  def initialize_skus
-    skus = {}
+# # +------+-------+------------------------+
+# # | Item | Price | Special offers         |
+# # +------+-------+------------------------+
+# # | A    | 50    | 3A for 130, 5A for 200 |
+# # | B    | 30    | 2B for 45              |
+# # | C    | 20    |                        |
+# # | D    | 15    |                        |
+# # | E    | 40    | 2E get one B free      |
+# # +------+-------+------------------------+
+#   def initialize_skus
+#     skus = {}
 
-    skus[:A] = Sku.new(:A, 50, [Discount.new(5, 200), Discount.new(3, 130)], [])
-    skus[:B] = Sku.new(:B, 30, [Discount.new(2, 45)], [])
-    skus[:C] = Sku.new(:C, 20, [], [])
-    skus[:D] = Sku.new(:D, 15, [], [])
-    skus[:E] = Sku.new(:E, 40, [], [Gift.new(2, 'B')])
+#     skus[:A] = Sku.new(:A, 50, [Discount.new(5, 200), Discount.new(3, 130)], [])
+#     skus[:B] = Sku.new(:B, 30, [Discount.new(2, 45)], [])
+#     skus[:C] = Sku.new(:C, 20, [], [])
+#     skus[:D] = Sku.new(:D, 15, [], [])
+#     skus[:E] = Sku.new(:E, 40, [], [Gift.new(2, 'B')])
 
-    @supported_skus = skus
-  end
+#     @supported_skus = skus
+#   end
 
-  def add_items(sku, amount)
-    items[sku.name] ||= Item.new(0, sku)
-    items[sku.name].amount += amount
-  end
+#   def add_items(sku, amount)
+#     items[sku.name] ||= Item.new(0, sku)
+#     items[sku.name].amount += amount
+#   end
 
-  def remove_items(sku, amount)
-      items[sku.name] ||= Item.new(0, sku)
-      current_amount = items[sku.name].amount
-      items[sku.name].amount = max(current_amount - amount, 0)
-  end
+#   def remove_items(sku, amount)
+#       items[sku.name] ||= Item.new(0, sku)
+#       current_amount = items[sku.name].amount
+#       items[sku.name].amount = max(current_amount - amount, 0)
+#   end
 
-  def calculate_special_offers
-    items.each do |name, item|
-      item.sku.apply_discount
-      item.sku.apply_gifts
-    end
-    # final_value += (item_amount / sku[:special_offers][:amount]) * sku[:special_offers][:price]
-    # final_value += (item_amount % sku[:special_offers][:amount]) * sku[:price]
-  end
+#   def calculate_total
+#     items.each do |name, item|
+#       item.sku.apply_special_offers(items)
+#     end
 
-  def calculate_discounts
-  end
+#     items.sum(&:total)
 
-  def calculate_gifts
-  end
-end
+#     # final_value += (item_amount / sku[:special_offers][:amount]) * sku[:special_offers][:price]
+#     # final_value += (item_amount % sku[:special_offers][:amount]) * sku[:price]
+#   end
+# end
+
 
