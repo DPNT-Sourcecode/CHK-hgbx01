b602941c7cd949871fb1e86e65f33665ca779234
sbx_user1051 <sbx_user1051@169.254.35.37>
2024-06-12 00:31:10 +0000

    Wed Jun 12 00:31:10 UTC 2024

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,42 +1,44 @@
 # # noinspection RubyUnusedLocalVariable
-# class Checkout
+class Checkout
 
-#   PRICE_TABLE = {
-#     A: { price: 50, special_offers: { discounts: [ { amount: 5, price: 200 }, { amount: 3, price: 130 } ] } },
-#     B: { price: 30, special_offers: { discounts: [ { amount: 2, price: 45 } ] } },
-#     C: { price: 20 },
-#     D: { price: 15 },
-#     E: { price: 40, special_offers: { gifts: [ { amount: 2, gift: :B } ] } },
-#   }
+  # This is too complex. Maybe it is better to make add OOP...
+  PRICE_TABLE = {
+    A: { price: 50, special_offers: { discounts: [ { amount: 5, price: 200 }, { amount: 3, price: 130 } ] } },
+    B: { price: 30, special_offers: { discounts: [ { amount: 2, price: 45 } ] } },
+    C: { price: 20 },
+    D: { price: 15 },
+    E: { price: 40, special_offers: { gifts: [ { amount: 2, gift: :B } ] } },
+  }
 
-#   def checkout(skus)
-#     check = {}
-#     final_value = 0
+  def checkout(skus)
+    check = {}
+    final_value = 0
 
-#     PRICE_TABLE.each do |item,  data|
-#       check[item] || = { total_price: 0 }
-#       check[item][:amount] = skus.count item.to_s
+    PRICE_TABLE.each do |item,  data|
+      check[item] || = { total_price: 0 }
+      check[item][:amount] = skus.count item.to_s
 
-#       if data.key? :special_offers
-#         calculate_special_offers(check)
-#       else
-#         check[item][:total_price] += data[:price] * check[item][:amount]
-#       end
-#     end
+      if data.key? :special_offers
+        calculate_special_offers(check)
+      else
+        check[item][:total_price] += data[:price] * check[item][:amount]
+      end
+    end
 
-#     (check.sum(&:amount) == skus.length) ? check.sum(&:total_price) : -1
-#   end
+    (check.sum(&:amount) == skus.length) ? check.sum(&:total_price) : -1
+  end
 
-#   private
+  private
 
-#   def calculate_special_offers
-#     # final_value += (item_amount / data[:special_offers][:amount]) * data[:special_offers][:price]
-#     # final_value += (item_amount % data[:special_offers][:amount]) * data[:price]
-#   end
+  def calculate_special_offers
+    # final_value += (item_amount / data[:special_offers][:amount]) * data[:special_offers][:price]
+    # final_value += (item_amount % data[:special_offers][:amount]) * data[:price]
+  end
 
-#   def calculate_discounts
-#   end
+  def calculate_discounts
+  end
 
-#   def calculate_gifts
-#   end
-# end
+  def calculate_gifts
+  end
+end
+
